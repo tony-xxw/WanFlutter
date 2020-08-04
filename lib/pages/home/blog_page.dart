3697f3d2c60cd5a21f88f001b21e11a70ccdc33a
tony-xxw <a664966415@gmail.com>
2020-08-05 00:35:30 +0800

    首页列表样式

--- lib/pages/home/blog_page.dart
+++ lib/pages/home/blog_page.dart
@@ -12,16 +12,51 @@ class _BlogPageState extends State<BlogPage> {
       itemBuilder: (_, index) {
         return buildBlogItem();
       },
-      itemExtent: 48.0,
-      itemCount: 4,
+      itemExtent: 110.0,
+      itemCount: 2,
     );
   }
 
   Widget buildBlogItem() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15.0),
-      child: Flex(
-        children: <Widget>[],
+      child: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ClipOval(
+              child: SizedBox(
+                width: 45.0,
+                height: 45.0,
+                child: Image(
+                  image: NetworkImage(
+                      'https://upload.jianshu.io/users/upload_avatars/3018771/a6cf120c-5efc-4ecd-8257-1bfafe48b0ed.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Text("李现"),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: Text("Android高级开发工程师"),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -21,7 +21,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   final List<String> imgList = [
     'https://pics3.baidu.com/feed/63d0f703918fa0ecbebca9de48f04ae93c6ddb5d.jpeg?token=5867474e1fcc999dedf1cccbbcf18df3',
     'https://pics4.baidu.com/feed/d50735fae6cd7b892552a4e1644351a0d9330e00.jpeg?token=1b581f4ede4eed576535e851223f31da',
-    'https://pcdn.flutterchina.club/imgs/6-1.png',
+    'https://i0.hdslb.com/bfs/archive/4a1de628f4182a6a12b8ca2135441209c71b9a8a.jpg@880w_388h_1c_95q',
+    'https://i0.hdslb.com/bfs/archive/2166630deecf7b40d880de285d2ef0206747d15e.jpg@880w_388h_1c_95q',
   ];
 
   @override
@@ -40,71 +41,104 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SearchBar(
-        rightImg: 'common/ic_menu',
-      ),
-      body: child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        CarouselSlider(
-          options: CarouselOptions(),
-          items: imgList
-              .map((item) =>
-              Container(
-                child: Center(
-                  child: Image.network(item,
-                      fit: BoxFit.cover, width: 400.0, errorBuilder:
-                          (BuildContext context, Object exception,
-                          StackTrace stackTrace) {
-                        print("图片 " + exception.toString());
-                        return Text(exception.toString());
-                      }),
-                ),
-              ))
-              .toList(),
+        appBar: SearchBar(
+          rightImg: 'common/ic_menu',
         ),
-        TabBar(
-          isScrollable: true,
-          controller: _tabController,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorColor: Colors.amberAccent,
-          labelColor: Colors.amberAccent,
-          unselectedLabelColor: Colours.dark_text,
-          dragStartBehavior: DragStartBehavior.start,
-          onTap: (index) {
-            setState(() {
-              currentIndex = index;
-            });
-            _pageController.jumpToPage(index);
-          },
-          tabs: <Widget>[
-            Padding(
-              child: Text(
-                "热门博客",
-                style:
-                TextStyle(fontSize: currentIndex == 0 ? 18.0 : 14.0),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
               ),
-              padding: EdgeInsets.symmetric(vertical: 8.0),
+              items: itemBuilder(),
             ),
-            Padding(
-              child: Text(
-                "热门项目",
-                style:
-                TextStyle(fontSize: currentIndex == 1 ? 18.0 : 14.0),
-              ),
-              padding: EdgeInsets.symmetric(vertical: 8.0),
+            TabBar(
+              isScrollable: true,
+              controller: _tabController,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorColor: Colors.amberAccent,
+              labelColor: Colors.amberAccent,
+              unselectedLabelColor: Colours.dark_text,
+              dragStartBehavior: DragStartBehavior.start,
+              onTap: (index) {
+                setState(() {
+                  currentIndex = index;
+                });
+                _pageController.jumpToPage(index);
+              },
+              tabs: <Widget>[
+                Padding(
+                  child: Text(
+                    "热门博客",
+                    style: TextStyle(fontSize: currentIndex == 0 ? 18.0 : 14.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                ),
+                Padding(
+                  child: Text(
+                    "热门项目",
+                    style: TextStyle(fontSize: currentIndex == 1 ? 18.0 : 14.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                )
+              ],
+            ),
+            Expanded(
+              flex: 1,
+              child: PageView.builder(
+                  onPageChanged: _pageChange,
+                  controller: _pageController,
+                  itemBuilder: (_, index) => _listTabView[index]),
             )
           ],
-        ),
-        Expanded(
-          flex: 1,
-          child: PageView.builder(
-              onPageChanged: _pageChange,
-              controller: _pageController,
-              itemBuilder: (_, index) => _listTabView[index]),
-        )
-      ],
-    ));
+        ));
+  }
+
+  List<Widget> itemBuilder() {
+    return imgList
+        .map((item) => Container(
+              child: Container(
+                margin: EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Text(
+                              'No. ${imgList.indexOf(item)} image',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
   }
 
   void _pageChange(index) {
