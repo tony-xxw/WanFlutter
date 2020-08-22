08fbab451a4e89e7b1dfe7f564e9a46fd1e4ec96
向鲜汶 <xiangxianwen@dianmi365.com>
2020-08-22 18:28:25 +0800

    我的页面

--- lib/pages/mine/profile.dart
+++ lib/pages/mine/profile.dart
@@ -8,7 +8,13 @@ import 'package:wanflutter/widgets/load_image.dart';
 
 class Profile extends StatefulWidget {
   final double spreadHeight = 200.0;
-  final List<String> mines = <String>["我收藏的", "我订阅的"];
+  final List<String> mines = <String>["TODO", "我的积分", "我的收藏", "我的分享"];
+  final List<String> icons = <String>[
+    "main/ic_todo",
+    "main/ic_integral",
+    "main/ic_collection",
+    "ic_ic_share"
+  ];
 
   @override
   _Profile createState() => _Profile();
@@ -17,14 +23,8 @@ class Profile extends StatefulWidget {
 class _Profile extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    print("Size:  ${MediaQuery
-        .of(context)
-        .size
-        .width} ");
-    final double statusBarHeight = MediaQuery
-        .of(context)
-        .padding
-        .top;
+    print("Size:  ${MediaQuery.of(context).size.width} ");
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -45,7 +45,7 @@ class _Profile extends State<Profile> {
                 final double t = (appBarHeight - kToolbarHeight) /
                     (widget.spreadHeight - kToolbarHeight);
                 final Curve _textOpacity =
-                const Interval(0.1, 1.0, curve: Curves.easeInOut);
+                    const Interval(0.1, 1.0, curve: Curves.easeInOut);
 
                 return Container(
                   child: Stack(
@@ -53,10 +53,7 @@ class _Profile extends State<Profile> {
                       Container(
                         child: LoadAssetImage(
                           "main/ic_mine",
-                          width: MediaQuery
-                              .of(context)
-                              .size
-                              .width,
+                          width: MediaQuery.of(context).size.width,
                           fit: BoxFit.fitWidth,
                         ),
                       ),
@@ -114,40 +111,42 @@ class _Profile extends State<Profile> {
                           height: biggerHeight,
                           child: ClipRect(
                               child: BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 2,
-                                  sigmaY: 2,
-                                ),
-                                child: Column(
+                            filter: ImageFilter.blur(
+                              sigmaX: 2,
+                              sigmaY: 2,
+                            ),
+                            child: Column(
+                              children: [
+                                Row(
                                   children: [
-                                    Row(
-                                      children: [
-                                        SizedBox(
-                                          width: 10,
-                                        ),
-                                        CircleAvatar(
-                                          radius: 18,
-                                          backgroundImage: AssetImage(
-                                              'assets/images/common/ic_avatar.png'),
-                                        ),
-                                        SizedBox(
-                                          width: 20,
-                                        ),
-                                        Text(
-                                          "Wynne",
-                                          style: TextStyle(
-                                              color: Colours.bg_white,
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
+                                    SizedBox(
+                                      width: 10,
                                     ),
-                                    SizedBox(height: 10,)
+                                    CircleAvatar(
+                                      radius: 18,
+                                      backgroundImage: AssetImage(
+                                          'assets/images/common/ic_avatar.png'),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      "Wynne",
+                                      style: TextStyle(
+                                          color: Colours.bg_white,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    )
                                   ],
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.end,
                                 ),
-                              )),
+                                SizedBox(
+                                  height: 10,
+                                )
+                              ],
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                            ),
+                          )),
                         )
                     ],
                   ),
@@ -157,13 +156,30 @@ class _Profile extends State<Profile> {
           ),
           SliverList(
               delegate: SliverChildBuilderDelegate((context, index) {
-                return Container(
-                  color: Color.fromARGB(255, Random().nextInt(256) + 0,
-                      Random().nextInt(256) + 0, Random().nextInt(256) + 0),
-                  child: Text(widget.mines[index]),
-                  height: 50,
-                );
-              }, childCount: widget.mines.length))
+            return Container(
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(width: 0.3,color: Colours.bg_gray))
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  LoadAssetImage(
+                    widget.icons[index],
+                    width: 24,
+                    height: 24,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(widget.mines[index],style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14),),
+                ],
+              ),
+              height: 60,
+            );
+          }, childCount: widget.mines.length))
         ],
       ),
     );
--- lib/pages/system/system.dart
+++ lib/pages/system/system.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:wanflutter/res/colours.dart';
+import 'package:wanflutter/widgets/load_image.dart';
 
 class System extends StatefulWidget {
   @override
@@ -9,8 +11,29 @@ class _System extends State<System> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: <Widget>[Text("体系")],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          LoadAssetImage(
+            "main/ic_mine",
+            width: 100,
+            height: 100,
+            fit: BoxFit.fitWidth,
+          ),
+          Container(
+            color: Colours.bg_blue,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("1111"),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("222"),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
